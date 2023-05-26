8a73fa949de0c913f15c2ef04fbd1b90738e9a46
morlen <lennart.moritz@iosb.fraunhofer.de>
2023-05-26 18:41:39 +0200

    add first ds downsampling

--- run_whisper.py
+++ run_whisper.py
@@ -7,47 +7,32 @@ import torch
 from evaluate import load
 import sys
 from tqdm import tqdm
-
-def transcribe():
-    # load model and processor
-    processor = WhisperProcessor.from_pretrained("openai/whisper-base")
-    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base")
-    model.config.forced_decoder_ids = None
-
-    # load dummy dataset and read audio files
-    ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-    sample = ds[0]["audio"]
-    input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
-
-    # generate token ids
-    predicted_ids = model.generate(input_features)
-    # decode token ids to text
-    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
-    print(transcription)
-
-    print("----------------------------------------------")
-
-    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-    print(transcription)
+import numpy as np
 
 def evaluate():
-    librispeech_test_clean = load_dataset("librispeech_asr", "clean", split="test")
+    # librispeech_test_clean = load_dataset("librispeech_asr", "clean", split="test")
+    librispeech_test_clean = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
     print(librispeech_test_clean["audio"][0])
+    """ This prints:
+    {'path': '/home/lennux/.cache/huggingface/datasets/downloads/extracted/80f773ffef1f9e7c284a356bb99db2f740655f871257acd846751dde70987539/dev_clean/1272/128104/1272-128104-0000.flac', 'array': array([0.00238037, 0.0020752 , 0.00198364, ..., 0.00042725, 0.00057983,
+       0.0010376 ]), 'sampling_rate': 16000}
+
+    """
     print(len(librispeech_test_clean["audio"][0]["array"]))
-    print(type(librispeech_test_clean["audio"][0]["array"]))
+    print(type(librispeech_test_clean["audio"][0]["array"][0]))
     # librispeech_test_clean = librispeech_test_clean.map(resample_audio)
-    librispeech_test_clean = resample_audio(librispeech_test_clean)
+    # librispeech_test_clean = resample_audio(librispeech_test_clean)
     # sys.exit()
 
     # processor = WhisperProcessor.from_pretrained("openai/whisper-base")
     ds_feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-base")
     # ds_feature_extractor = WhisperFeatureExtractor() # TODO: delete line
     ds_factor = 2
-    ds_feature_extractor.n_fft = ds_feature_extractor.n_fft // ds_factor
+    ds_feature_extractor.n_fft = ds_feature_extractor.n_fft
     ds_feature_extractor.sampling_rate = ds_feature_extractor.sampling_rate // ds_factor
-    ds_feature_extractor.hop_length = ds_feature_extractor.hop_length // ds_factor
+    ds_feature_extractor.hop_length = ds_feature_extractor.hop_length // 1
     ds_feature_extractor.mel_filters = mel_filter_bank(
-            num_frequency_bins=1 + ds_feature_extractor.n_fft // ds_factor,
+            num_frequency_bins=1 + ds_feature_extractor.n_fft // 2,
             num_mel_filters=ds_feature_extractor.feature_size,
             min_frequency=0.0,
             max_frequency=8000.0 // ds_factor,
@@ -63,8 +48,16 @@ def evaluate():
     model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base").to("cuda")
 
     def map_to_pred(batch):
+        print(len(batch))
+        print(len(batch["audio"]))
         audio = batch["audio"]
-        input_features = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
+        # input_features = processor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
+        my_temp_audio = batch["audio"]["array"].copy()
+        print(f"BEFORE: {len(my_temp_audio)}")
+        my_temp_audio = resample(my_temp_audio)
+        print(f"AFTER: {len(my_temp_audio)}")
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        input_features = processor(my_temp_audio, sampling_rate=8000, return_tensors="pt").input_features
         batch["reference"] = processor.tokenizer._normalize(batch['text'])
 
         with torch.no_grad():
@@ -81,10 +74,43 @@ def evaluate():
 
 # Custom function to downsample audio to 8 kHz
 def resample_audio(example, ds_factor = 2):
-    for index in tqdm(range(len(example["audio"]))):
+    for index in tqdm(range(30, len(example["audio"]))):
         example["audio"][index]["sampling_rate"] = example["audio"][index]["sampling_rate"] // ds_factor
-        example["audio"][index]["array"] = example["audio"][index]["array"][::2]
+        # example["audio"][index]["array"] = example["audio"][index]["array"][::2]
+        if len(example["audio"][index]["array"]) % 2:
+            print(len(example["audio"][index]["array"])) # this is 39119
+            example["audio"][index]["array"] = example["audio"][index]["array"][:-1].copy()
+            print(len(example["audio"][index]["array"][:-1])) # this is 39118
+
+            # example["audio"][index]["array"] = example["audio"][index]["array"][np.arange(example["audio"][index]["array"].size - 1)]
+            temp = example["audio"][index]["array"][np.arange(example["audio"][index]["array"].size - 1)]
+            del(example["audio"][index]["array"])
+            example["audio"][index]["array"] = None
+            print(example["audio"][index]["array"])
+            example["audio"][index]["array"] = temp
+            
+            print("+-+-+-+-+-+-++-+-+-+-+-++-++-+-+-+-++--+-++-+-+-+-+-YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            print(len(example["audio"][index]["array"])) # this is 39119
+        example["audio"][index]["array"] = example["audio"][index]["array"].reshape(-1, 2)[:,1].flatten()
     return example
+
+def resample(x):
+     if len(x) % 2:
+             x = x[:-1]
+     x = x.reshape(-1,2)[:,:1].flatten()
+     return x
+
+def test_resample():
+    array_list = [np.random.normal(loc=0, scale=0.04, size=np.random.randint(32000, 448000))
+                  for _ in range(2620)]
+    print(array_list[0][:6])
+    print(type(array_list[0][1]))
+    with tqdm(total=len(array_list), desc="Resampling") as pbar:
+        for i in range(len(array_list)):
+            array_list[i] = resample(array_list[i])
+            pbar.update(1)
+    print(array_list[0][:6])
 
 if __name__ == "__main__":
     evaluate()
+    # test_resample()
